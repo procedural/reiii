b189f958447b55a58fbf44e57304df5fc9f03762
procedural <constantine@protonmail.com>
2020-04-08 10:33:57 +0300

    Add missing command ids to instance procedures

--- reiii.h
+++ reiii.h
@@ -10,8 +10,8 @@ void iiiTriangle(unsigned id,
   float x0, float y0, float z0, float w0, float r0, float g0, float b0, float a0, float u0x, float v0y, float u0z, float v0w, float n0x, float n0y, float n0z, float n0w,
   float x1, float y1, float z1, float w1, float r1, float g1, float b1, float a1, float u1x, float v1y, float u1z, float v1w, float n1x, float n1y, float n1z, float n1w,
   float x2, float y2, float z2, float w2, float r2, float g2, float b2, float a2, float u2x, float v2y, float u2z, float v2w, float n2x, float n2y, float n2z, float n2w);
-void iiiInstances(unsigned x, unsigned y, unsigned w, unsigned h, unsigned clearScreenColor, unsigned clearScreenDepth, float clearScreenDepthValue, const void * pipelineAndSamplersState, unsigned pixelsIndexOffsetForUvXy, unsigned pixelsIndexOffsetForUvZw);
-void iiiInstance(unsigned trianglesIndex, unsigned parametersOffset);
+void iiiInstances(unsigned id, unsigned x, unsigned y, unsigned w, unsigned h, unsigned clearScreenColor, unsigned clearScreenDepth, float clearScreenDepthValue, const void * pipelineAndSamplersState, unsigned pixelsIndexOffsetForUvXy, unsigned pixelsIndexOffsetForUvZw);
+void iiiInstance(unsigned id, unsigned trianglesIndex, unsigned parametersOffset);
 void iiiEnd(unsigned id);
 void iiiParameters(void * parameters);
 void iiiSend(unsigned idsCount, const unsigned * ids);
