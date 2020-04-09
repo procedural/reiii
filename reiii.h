7812ef6a7b2dc4cbdc8344e0fd43a82505e506cc
procedural <constantine@protonmail.com>
2020-04-09 14:39:06 +0300

    Generalize vertex attributes

--- reiii.h
+++ reiii.h
@@ -5,11 +5,11 @@ void iiiScreen(unsigned w, unsigned h, const void * displayOrHinstance, const vo
 void iiiBegin(unsigned id);
 void iiiPixels(unsigned id, unsigned pixelsIndex, unsigned w, unsigned h, unsigned isCube, unsigned clearPixels);
 void iiiPixel(unsigned id, unsigned x, unsigned y, unsigned z, unsigned char r, unsigned char g, unsigned char b, unsigned char a);
-void iiiTriangles(unsigned id, unsigned trianglesIndex, unsigned enableColor, unsigned enableUvs, unsigned enableNormal);
+void iiiTriangles(unsigned id, unsigned trianglesIndex, unsigned va1Enabled, unsigned va2Enabled, unsigned va3Enabled);
 void iiiTriangle(unsigned id,
-  float x0, float y0, float z0, float w0, float r0, float g0, float b0, float a0, float u0x, float v0y, float u0z, float v0w, float n0x, float n0y, float n0z, float n0w,
-  float x1, float y1, float z1, float w1, float r1, float g1, float b1, float a1, float u1x, float v1y, float u1z, float v1w, float n1x, float n1y, float n1z, float n1w,
-  float x2, float y2, float z2, float w2, float r2, float g2, float b2, float a2, float u2x, float v2y, float u2z, float v2w, float n2x, float n2y, float n2z, float n2w);
+  float v0a0x, float v0a0y, float v0a0z, float v0a0w, float v0a1x, float v0a1y, float v0a1z, float v0a1w, float v0a2x, float v0a2y, float v0a2z, float v0a2w, float v0a3x, float v0a3y, float v0a3z, float v0a3w,
+  float v1a0x, float v1a0y, float v1a0z, float v1a0w, float v1a1x, float v1a1y, float v1a1z, float v1a1w, float v1a2x, float v1a2y, float v1a2z, float v1a2w, float v1a3x, float v1a3y, float v1a3z, float v1a3w,
+  float v2a0x, float v2a0y, float v2a0z, float v2a0w, float v2a1x, float v2a1y, float v2a1z, float v2a1w, float v2a2x, float v2a2y, float v2a2z, float v2a2w, float v2a3x, float v2a3y, float v2a3z, float v2a3w);
 void iiiInstances(unsigned id, unsigned x, unsigned y, unsigned w, unsigned h, unsigned clearScreenColor, unsigned clearScreenDepth, float clearScreenDepthValue, const void * pipelineAndSamplersState, const unsigned * pixelsIndexes);
 void iiiInstance(unsigned id, unsigned trianglesIndex, unsigned parametersOffset);
 void iiiEnd(unsigned id);
