bfae4aedbad683f8e5a93cdb589af6bec80bec1f
procedural <constantine@protonmail.com>
2020-04-10 06:56:52 +0300

    No need to open or close command ids manually

--- reiii.h
+++ reiii.h
@@ -2,7 +2,6 @@
 
 void iiiHash(uint64_t hash);
 void iiiScreen(unsigned w, unsigned h, const void * displayOrHinstance, const void * windowOrHwnd);
-void iiiBegin(unsigned id);
 void iiiPixels(unsigned id, unsigned pixelsIndex, unsigned w, unsigned h, unsigned isCube, unsigned clearPixels);
 void iiiPixel(unsigned id, unsigned x, unsigned y, unsigned z, unsigned char r, unsigned char g, unsigned char b, unsigned char a);
 void iiiTriangles(unsigned id, unsigned trianglesIndex, unsigned enabledVertexAttributesCount);
@@ -12,7 +11,6 @@ void iiiTriangle(unsigned id,
   float v2a0x, float v2a0y, float v2a0z, float v2a0w, float v2a1x, float v2a1y, float v2a1z, float v2a1w, float v2a2x, float v2a2y, float v2a2z, float v2a2w, float v2a3x, float v2a3y, float v2a3z, float v2a3w);
 void iiiInstances(unsigned id, unsigned x, unsigned y, unsigned w, unsigned h, unsigned clearScreenColor, unsigned clearScreenDepth, float clearScreenDepthValue, const void * pipelineAndSamplersState, const unsigned * pixelsIndexes);
 void iiiInstance(unsigned id, unsigned trianglesIndex, unsigned parametersOffset);
-void iiiEnd(unsigned id);
 void iiiParameters(void * parameters);
 void iiiSend(unsigned idsCount, const unsigned * ids);
 void iiiReadback(unsigned x, unsigned y, unsigned w, unsigned h, unsigned char * outPixels);
